cfbf2c4654c8616baaecba4ee774c5814b142738
Dean <hl2guide@gmail.com>
2023-08-31 11:47:11 +0000

    update

--- create_list.py
+++ create_list.py
@@ -4,6 +4,8 @@
 # removes comment lines, sorts all lines and then makes lines unique
 
 from urllib.request import urlretrieve
+import glob
+import os
 
 URLS = ["https://blocklistproject.github.io/Lists/adguard/crypto-ags.txt",
         "https://blocklistproject.github.io/Lists/adguard/drugs-ags.txt",
@@ -13,28 +15,53 @@ URLS = ["https://blocklistproject.github.io/Lists/adguard/crypto-ags.txt",
         "https://blocklistproject.github.io/Lists/adguard/ransomware-ags.txt",
         "https://blocklistproject.github.io/Lists/adguard/scam-ags.txt",
         "https://blocklistproject.github.io/Lists/adguard/tracking-ags.txt",
-        "https://blocklistproject.github.io/Lists/adguard/abuse-ags.txt",
+        "https://blocklistproject.github.io/Lists/adguard/abuse-ags.txt"
         # Anudeep's Blacklist
         #"https://hosts.anudeep.me/mirror/adservers.txt",
         # LostAd
-        "https://raw.githubusercontent.com/lennihein/LostAd/main/lostad_dns.txt",
+        #"https://raw.githubusercontent.com/lennihein/LostAd/main/lostad_dns.txt",
         # ppfeufer / adguard-filter-list
-        "https://raw.githubusercontent.com/ppfeufer/adguard-filter-list/master/blocklist",
+        #"https://raw.githubusercontent.com/ppfeufer/adguard-filter-list/master/blocklist",
         # HaGeZi's Pro DNS Blocklist
-        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt",
+        #"https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt",
         # HaGeZi's The World's Most Abused TLDs
-        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds.txt",
+        #"https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds.txt",
         # Steven Black's List
-        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt",
+        #"https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt",
         # OISD Blockist Big
-        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt"
+        #"https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt"
         ]
 
 # Downloads the individual lists
 for URL in URLS:
     LIST_INDEX = URLS.index(URL)
     FILENAME = "downloaded_lists/blocklist" + str(LIST_INDEX) + ".txt"
-    urlretrieve(URL, FILENAME)
     print("Downloading: "+ FILENAME)
+    urlretrieve(URL, FILENAME)
 
 # Merges lists into one file
+# Source: https://bobbyhadz.com/blog/merge-text-files-in-python#how-to-merge-text-files-in-python
+file_paths = glob.glob('downloaded_lists/*.txt')
+with open('aio_blocklist.txt', 'w', encoding='utf-8') as output_file:
+    for file_path in file_paths:
+        with open(file_path, 'r', encoding='utf-8') as input_file:
+            output_file.write(input_file.read() + '\n')
+
+# Removes individual text files
+I = 0
+while I < len(URLS):
+    print("Removing: downloaded_lists/blocklist"+str(I)+".txt")
+    os.remove("downloaded_lists/blocklist"+str(I)+".txt")
+    I = I + 1
+
+# Clean aio.txt file
+print("Cleaning aio_blocklist.txt, please wait..")
+UNIQUELINES = set(open('aio_blocklist.txt', encoding='utf-8').readlines())
+CLEANEDOUTPUT = open('aio_blocklist.txt', 'w', encoding='utf-8').writelines(UNIQUELINES)
+AIOFILE = open('aio_blocklist.txt', encoding='utf-8')
+with AIOFILE as file:
+    for line in file:
+        if line.startswith('#'):
+            continue  # skip comments
+        line = line.strip()
+CLEANEDOUTPUT = open('aio_blocklist.txt', 'w', encoding='utf-8').writelines(UNIQUELINES)
