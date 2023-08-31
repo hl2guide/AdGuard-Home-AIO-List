0549453db89049e4d4e5dd3803e4621dd8b48208
Dean <hl2guide@gmail.com>
2023-08-31 13:03:01 +0000

    update

--- create_list.py
+++ create_list.py
@@ -8,6 +8,8 @@ import glob
 import os
 from time import gmtime, strftime
 
+NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
 URLS = ["https://blocklistproject.github.io/Lists/adguard/crypto-ags.txt",
         "https://blocklistproject.github.io/Lists/adguard/drugs-ags.txt",
         "https://blocklistproject.github.io/Lists/adguard/fraud-ags.txt",
@@ -27,18 +29,25 @@ URLS = ["https://blocklistproject.github.io/Lists/adguard/crypto-ags.txt",
         "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt",
         # HaGeZi's The World's Most Abused TLDs
         "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds.txt",
+        # HaGeZi's Gambling DNS Blocklist
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/gambling.txt",
         # Steven Black's List
         "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt",
         # OISD Blockist Big
-        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt"
+        "https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt",
+        # AdGuard Simplified Domain Names filter
+        "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
         ]
+
+NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 # Downloads the individual lists
 for URL in URLS:
     LIST_INDEX = URLS.index(URL)
     FILENAME = "downloaded_lists/blocklist" + str(LIST_INDEX) + ".txt"
-    print("Downloading: "+ FILENAME)
+    print(NOW+" - "+"Downloading: "+ FILENAME)
     urlretrieve(URL, FILENAME)
+    NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 # Merges lists into one file
 # Source: https://bobbyhadz.com/blog/merge-text-files-in-python#how-to-merge-text-files-in-python
@@ -48,15 +57,19 @@ with open('aio_blocklist.txt', 'w', encoding='utf-8') as output_file:
         with open(file_path, 'r', encoding='utf-8') as input_file:
             output_file.write(input_file.read() + '\n')
 
+NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
 # Removes individual text files
 I = 0
 while I < len(URLS):
-    print("Removing: downloaded_lists/blocklist"+str(I)+".txt")
+    print(NOW+" - "+"Removing: downloaded_lists/blocklist"+str(I)+".txt")
     os.remove("downloaded_lists/blocklist"+str(I)+".txt")
     I = I + 1
 
+NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
 # Cleans aio_blocklist.txt file
-print("Cleaning aio_blocklist.txt, please wait..")
+print(NOW+" - "+"Cleaning aio_blocklist.txt, please wait..")
 # Open the file
 UNIQUELINES = set(open('aio_blocklist.txt', 'r', encoding='utf-8').readlines())
 # Sort all lines in the set
@@ -64,8 +77,10 @@ UNIQUELINES = sorted(UNIQUELINES)
 # Removes duplicates from from text file
 CLEANEDOUTPUT = open('aio_blocklist.txt', 'w', encoding='utf-8').writelines(UNIQUELINES)
 
+NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
 # Removes all junk from aio_blocklist.txt file
-print("Removing junk lines from aio_blocklist.txt, please wait..")
+print(NOW+" - "+"Removing junk lines from aio_blocklist.txt, please wait..")
 with open("aio_blocklist.txt", "r", encoding='utf-8') as f:
     LINES = f.readlines()
 with open("aio_blocklist_final.txt", "w", encoding='utf-8') as new_f:
@@ -73,6 +88,6 @@ with open("aio_blocklist_final.txt", "w", encoding='utf-8') as new_f:
         if not line.startswith("!") or line.startswith("#") or line.startswith("@@"):
             new_f.write(line)
 os.remove("aio_blocklist.txt")
-print("AIO list has been generated.")
 
 NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+print(NOW+" - "+"AIO list has been generated.")
